478b08d6dd38bef3285acb59c12c3d790803aa64
Admin <mark@uuu.com>
2024-02-22 17:17:26 +0800

    改成用DB

--- src/main/java/com/uuu/ddd/demo/demo1/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/uuu/ddd/demo/demo1/infrastructure/configuration/BeanConfiguration.java
@@ -3,6 +3,7 @@ package com.uuu.ddd.demo.demo1.infrastructure.configuration;
 import com.uuu.ddd.demo.demo1.domain.common.repository.PointTypeRepository;
 import com.uuu.ddd.demo.demo1.domain.common.service.PointTypeService;
 import com.uuu.ddd.demo.demo1.domain.common.service.PointTypeServiceImpl;
+import com.uuu.ddd.demo.demo1.infrastructure.repository.PointTypeDBRepository;
 import com.uuu.ddd.demo.demo1.infrastructure.repository.PointTypeMemRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,8 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
     @Bean
     PointTypeRepository pointTypeRepository() {
-        return new PointTypeMemRepository();
+        //return new PointTypeMemRepository();
+        return new PointTypeDBRepository();
     }
     @Bean
     PointTypeService pointTypeService(PointTypeRepository repository) {
